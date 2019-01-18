9baf1b82533639ea8fd1fb40b2624fdcd44def83
tianxiang374 <479294403@qq.com>
2019-01-18 23:42:33 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <router-view/>
+    <FooterGuide v-show="$route.meta.showFooter"/><!--$route当前路由，$router路由控制器-->
+  </div>
+</template>
+
+<script>
+  import FooterGuide from "./components/FooterGuide/FooterGuide.vue";
+    export default {
+      components:{
+        FooterGuide
+      }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background-color  #f5f5f5
+</style>
